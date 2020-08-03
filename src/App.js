c3b2554c36ab58537b08b00b01988cb72877989e
staticriou <riyonugroho@outlook.com>
2020-08-04 05:11:22 +0700

    mengupload react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import UserInfo from './tugas11/UserInfo.js';
+
+function App() {
+  return (
+  	<div>
+      <UserInfo/>
+    </div>
+  );
+}
+
+
+export default App;
