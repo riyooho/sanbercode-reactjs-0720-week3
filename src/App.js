10dcc4bdb2e6f3fa5266d48bbd0dc766112a9412
staticriou <riyonugroho@outlook.com>
2020-08-06 07:16:04 +0700

    mengupload tugas hari ke-13 (react)

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import TabelBuah from './tugas11/TabelBuah.js';
 import ClockCount from './tugas12/ClockCount.js';
+import Lists from './tugas13/Lists.js';
 
 function App() {
   return (
   	<div align="center">
-      <h1>Tabel Harga Buah</h1>
-      <TabelBuah/>
-	  <ClockCount startCount = {101}/>      
+  	<h1>Tabel Harga Buah</h1>
+  	<TabelBuah/>
+  	<ClockCount startCount = {100}/>
+	<Lists/>
     </div>
   );
 }
